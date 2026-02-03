04ca4e96b302f84c58c53fbafa82ca5f13670829
whatbee <gabe.bf@gmail.com>
2026-02-03 19:36:50 +0000

    add negative tests

--- tests/test_basic_output.py
+++ tests/test_basic_output.py
@@ -1,6 +1,8 @@
 import json
-import pytest
 from pathlib import Path
+
+import pytest
+
 from FracturedJSON import Encoder
 
 params = [
@@ -9,11 +11,20 @@ params = [
     ("indent_2.json",{"indent":2}),
 ]
 
-@pytest.mark.parametrize("filename,kwargs",params)
-def test_roundtrip(filename:str,kwargs:dict):
+@pytest.mark.parametrize(("filename", "kwargs"), params)
+def test_roundtrip(filename:str, kwargs:dict):
     file = Path(__file__).parent / filename
     file_contents = file.read_text()
     data = json.loads(file_contents)
     
     formatted = json.dumps(data, cls=Encoder, **kwargs)
-    assert formatted ==  file_contents
+    assert formatted == file_contents
+
+@pytest.mark.parametrize(("filename", "kwargs"), params)
+def test_negative_roundtrip(filename:str, kwargs:dict):
+    file = Path(__file__).parent / filename
+    file_contents = file.read_text()
+    data = json.loads(file_contents)
+    
+    formatted = json.dumps(data)
+    assert formatted != file_contents
