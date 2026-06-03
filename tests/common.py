2034320f1250394728ede2e23c21bdb313966930
whatbee <gabe.bf@gmail.com>
2026-06-03 13:31:21 +0100

    add tests for new options `max_inline_complexity`, `max_compact_array_complexity`, `max_table_row_complexity`. `number_list_alignment`

--- tests/common.py
+++ tests/common.py
@@ -19,6 +19,23 @@ _params: list[paramType] = [
     ("default_settings.json", {}),
     ("line_len_100.json", {"line_length":100}),
     ("indent_2.json", {"indent":2}),
+
+    ("inline_complexity_-1.json", {"max_inline_complexity":-1}),
+    ("inline_complexity_0.json", {"max_inline_complexity":0}),
+    ("inline_complexity_1.json", {"max_inline_complexity":1}),
+
+    ("compact_array_complexity_-1.json", {"max_compact_array_complexity":-1}),
+    ("compact_array_complexity_0.json", {"max_compact_array_complexity":0}),
+    ("compact_array_complexity_1.json", {"max_compact_array_complexity":1}),
+
+    ("table_row_complexity_-1.json", {"max_table_row_complexity":-1}),
+    ("table_row_complexity_0.json", {"max_table_row_complexity":0}),
+    ("table_row_complexity_1.json", {"max_table_row_complexity":1}),
+    
+    ("number_list_alignment_left.json", {"number_list_alignment":"left"}),
+    ("number_list_alignment_decimal.json", {"number_list_alignment":"decimal"}),
+    ("number_list_alignment_right.json", {"number_list_alignment":"right"}),
+    ("number_list_alignment_normalize.json", {"number_list_alignment":"normalize"}),
 ]
 
 JSONS_DIR = "JSONs/"
@@ -30,8 +47,6 @@ def generate_jsons():
         file = "tests/" + filename
         with open(file, "w") as f:
             json.dump(file_dict, f, cls=Encoder, **kwargs)
-
-            json.dump(file_dict, f, cls=Encoder)
 
 if __name__ == "__main__":
     generate_jsons()
